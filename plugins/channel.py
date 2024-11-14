ec5a5756ead46dcf73d2838db5da62baa0053ca3
Noise tg <137701851+kunjava123@users.noreply.github.com>
2024-11-14 20:28:23 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -81,8 +81,6 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [[
             InlineKeyboardButton('📂 ɢᴇᴛ ғɪʟᴇ 📂', url=f'https://telegram.me/{temp.U_NAME}?start=getfile-{search_movie}')
-        ],[
-            InlineKeyboardButton('♻️ ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ♻️', url=f'https://t.me/JISSHU_BOTS')
         ]]
         reply_markup = InlineKeyboardMarkup(btn)
         if poster_url:
