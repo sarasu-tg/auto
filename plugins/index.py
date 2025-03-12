7cd204196ef45967774f557cb775e8cd0f74185e
MN - TG <mntg3509@gmail.com>
2025-03-12 21:52:57 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -171,15 +171,15 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                     continue
                 media.file_type = message.media.value
                 media.caption = message.caption
-                aynav, vnay = await save_file(media)
-                if aynav:
+                result = await save_file(media)
+                if result == 'suc':
                     total_files += 1
-                elif vnay == 0:
+                elif result == 'dup':
                     duplicate += 1
-                elif vnay == 2:
+                elif result == 'err':
                     errors += 1
         except Exception as e:
             logger.exception(e)
             await msg.edit(f'Error: {e}')
         else:
-            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
+            await msg.edit(f'Successfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
